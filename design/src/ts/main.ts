0ec8a17e0e00cfd0b6ef50a83ad79d58132f3c9e
Suhail Najmi <amocuk@gmail.com>
2022-09-01 13:42:25 +0800

    Pre abandon alpine implementation

--- design/src/ts/main.ts
+++ design/src/ts/main.ts
@@ -1,33 +1,33 @@
-import Alpine from 'alpinejs'
+// import Alpine from 'alpinejs'
 
 import '../css/index.css'
 // import { run as runService } from './services'
 
-let isDevelopment = !['127.0.0.1', 'localhost'].every((hostname: string) => !window.location.hostname.includes(hostname))
+// let isDevelopment = !['127.0.0.1', 'localhost'].every((hostname: string) => !window.location.hostname.includes(hostname))
 
-const pathnamePre = isDevelopment ? window.location.pathname.replace('/pages/', '') : window.location.pathname as string
-const pathname = pathnamePre.substring(0, pathnamePre.length-1) as string
+// const pathnamePre = isDevelopment ? window.location.pathname.replace('/pages/', '') : window.location.pathname as string
+// const pathname = pathnamePre.substring(0, pathnamePre.length-1) as string
 
 // Navbar data
-document.addEventListener('alpine:init', () => {
-    Alpine.data('navbarData', () => ({
-        notifications: 5,
-        currentPageName: pathname,
-    }))
-})
+// document.addEventListener('alpine:init', () => {
+//     Alpine.data('navbarData', () => ({
+//         notifications: 5,
+//         currentPageName: pathname,
+//     }))
+// })
 
-let pageScriptsMap = {
-    // 'services': runService,
-    'home': () => {},
-    'services': () => {},
-    'neworder': () => {},
-    'orders': () => {},
-    'subscriptions': () => {},
-    'refill': () => {},
-    'addfunds': () => {},
-    'account': () => {},
-}
+// let pageScriptsMap = {
+//     // 'services': runService,
+//     'home': () => {},
+//     'services': () => {},
+//     'neworder': () => {},
+//     'orders': () => {},
+//     'subscriptions': () => {},
+//     'refill': () => {},
+//     'addfunds': () => {},
+//     'account': () => {},
+// }
 
-pageScriptsMap[pathname as keyof typeof pageScriptsMap]()
+// pageScriptsMap[pathname as keyof typeof pageScriptsMap]()
 
-Alpine.start()
+// Alpine.start()
