09a30fd8e542fc95b458e6cd6415cf0454665617
Suhail Najmi <amocuk@gmail.com>
2022-09-03 19:54:08 +0800

    Neworder before transport feature

--- design/vite.config.js
+++ design/vite.config.js
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite'
 import twig from 'vite-plugin-twig'
 import FullReload from 'vite-plugin-full-reload'
+import Twig from 'twig';
 
 export default defineConfig({
   // ...
   plugins: [
-    twig(),
+    twig({
+      globals: {
+        "site": {
+          "name": "development",
+          "scripts": [
+            {
+              "src": "/123jquery.123.com"
+            },
+            {
+              "src": "js1"
+            },
+            {
+              "src": "notjque"
+            }
+          ]
+        }
+      }
+    }),
     FullReload(['templates/**/*.{twig,html}', 'pages/**/*.{twig,html}']),
   ]
 })
